333fb7316471967cb2cb59042a32d78c769c4c5e
Vishal Pandey <vishalpandey000090@gmail.com>
2026-05-02 00:06:51 +0530

    Time: 4 ms (13.16%), Space: 43.4 MB (32.47%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public String longestCommonPrefix(String[] strs){
+        
+        String ans = "";
+
+        for(int i = 0; i < strs[0].length(); i++){
+            char ch = strs[0].charAt(i);
+
+            boolean match = true;
+
+            for(int j = 1; j < strs.length; j++){  // ✅ fixed
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch){
+                    match = false;
+                    break;
+                }
+            }
+
+            if(match){
+                ans += ch;
+            } else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args){
+        Solution s = new Solution();
+        String[] strs = {"flower","flow","flight"};
+        String res = s.longestCommonPrefix(strs);
+        System.out.println(res);
+    }
+}
